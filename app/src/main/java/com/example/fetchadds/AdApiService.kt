7222d94fb8c6afeb85ddfa8d386c9468b4f840db
sumayyamunir1 <57375791+sumayyamunir1@users.noreply.github.com>
2023-07-17 11:29:10 +0200

    Update AdApiService.kt

--- app/src/main/java/com/example/fetchadds/AdApiService.kt
+++ app/src/main/java/com/example/fetchadds/AdApiService.kt
@@ -4,6 +4,7 @@ import retrofit2.Call
 import retrofit2.http.GET
 
 interface AdApiService {
+    // defines the API endpoints for fetching ads
     @GET("baldermork/6a1bcc8f429dcdb8f9196e917e5138bd/raw/discover.json")
     fun getAds(): Call<Ad>
 }
